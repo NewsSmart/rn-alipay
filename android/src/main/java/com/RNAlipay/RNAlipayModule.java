bb7ebd5a329ae3784462e523f1a5781e135b0d57
NewsSmart <1187437618@qq.com>
2016-12-09 19:29:28 +0800

    Update RNAlipayModule.java

--- android/src/main/java/com/RNAlipay/RNAlipayModule.java
+++ android/src/main/java/com/RNAlipay/RNAlipayModule.java
@@ -142,11 +142,9 @@ public class RNAlipayModule extends ReactContextBaseJavaModule {
 	  if (TextUtils.equals(resultStatus, "9000")) {
 		// 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
 		promise.resolve("支付成功");
-		promise.resolve(resultInfo);
 	  } else {
 		// 该笔订单真实的支付结果，需要依赖服务端的异步通知。
 		promise.resolve("支付失败");
-		promise.resolve(resultInfo);
 	  }
     }
 	
